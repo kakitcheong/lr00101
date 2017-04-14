d35f012bc1aad4e4a652f861f161801ea862b636
kakitcheong <slg-ice@slg-ice.local>
2017-04-14 16:48:23 +0800

    WIP : buidling gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat');
+
+var jsSources = [
+	'components/scripts/untitled.js'	
+];
 
 gulp.task('log', function() {
 	gutil.log('I am up and running');
+});
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
