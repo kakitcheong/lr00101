2cca91e7e09bdea9072414c0aebe1decd158aa4a
kakitcheong <slg-ice@slg-ice.local>
2017-04-17 14:41:18 +0800

    Added watch task

--- components/scripts/untitled.js
+++ components/scripts/untitled.js
@@ -1,1 +1,8 @@
+console.log('hello. I am up and running');
+
 console.log('hello');
+
+console.log('another line');
+
+console.log('hello again');
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	compass = require('gulp-compass');
 
 var jsSources = [
-	'components/scripts/untitled.js'	
+	'components/scripts/untitled.js'
 ];
+
+sassSources = ['components/sass/style.scss'];
 
 gulp.task('log', function() {
 	gutil.log('I am up and running');
@@ -15,3 +18,25 @@ gulp.task('js', function(){
 		.pipe(concat('script.js'))
 		.pipe(gulp.dest('builds/development/js'))
 });
+
+gulp.task('compass', function(){
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			css: 'stylesheets',
+			image: 'builds/development/images',
+			style: 'expanded',
+			require: [
+				'modular-scale'
+			]
+		})
+		.on('error', gutil.log))
+		.pipe(gulp.dest('builds/development/stylesheets'))
+});
+
+gulp.task('watch', function(){
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/**/*.scss', ['compass']);
+});
+
+// gulp.task('default', )
